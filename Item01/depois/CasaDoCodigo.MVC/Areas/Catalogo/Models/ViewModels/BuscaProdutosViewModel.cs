b3b57f69671314b498d6934aca5e70efddc0007d
marcelooliveira <mclricardo@gmail.com>
2020-02-25 21:52:23 -0300

    Item01\depois\CasaDoCodigo.MVC

---
+++ Item01/depois/CasaDoCodigo.MVC/Areas/Catalogo/Models/ViewModels/BuscaProdutosViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CasaDoCodigo.Areas.Catalogo.Models;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Areas.Catalogo.Models.ViewModels
+{
+    public class BuscaProdutosViewModel
+    {
+        public BuscaProdutosViewModel(List<Produto> produtos, string pesquisa)
+        {
+            Produtos = produtos;
+            Pesquisa = pesquisa;
+        }
+
+        public List<Produto> Produtos { get; }
+        public string Pesquisa { get; set; }
+    }
+}
