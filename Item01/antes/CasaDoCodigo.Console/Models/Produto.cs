9cd5357a3ef17b70684b43e8d47e404524338970
marcelooliveira <mclricardo@gmail.com>
2020-02-25 21:25:31 -0300

    Item01\antes\CasaDoCodigo.Console

---
+++ Item01/antes/CasaDoCodigo.Console/Models/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaDoCodigo.Areas.Catalogo.Models
+{
+    public class Produto : BaseModel
+    {
+        public Produto()
+        {
+
+        }
+
+        public int? CategoriaId { get; set; }
+
+        [Required]
+        public Categoria Categoria { get; set; }
+        [Required]
+        public string Codigo { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public decimal Preco { get; set; }
+
+        public Produto(int id, string codigo, string nome, decimal preco, Categoria categoria) 
+            : this(codigo, nome, preco, categoria)
+        {
+            Id = id;
+        }
+
+        public Produto(string codigo, string nome, decimal preco, Categoria categoria)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Categoria = categoria;
+        }
+    }
+}
---
+++ Item01/antes/CasaDoCodigo.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using CasaDoCodigo.Areas.Catalogo.Data;
+using CasaDoCodigo.Areas.Catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CasaDoCodigo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
