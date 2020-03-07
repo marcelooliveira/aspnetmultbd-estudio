d7098761942394324bca3cb1ea9c7a4a7720989a
marcelooliveira <mclricardo@gmail.com>
2020-03-07 12:06:37 -0300

    Item03 - Redis - inicio

---
+++ Item03/antes/CasaDoCodigo.MVC/Areas/Catalogo/ViewComponents/ProdutoCardViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using CasaDoCodigo.Areas.Catalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Areas.Catalogo.ViewComponents
+{
+    public class ProdutoCardViewComponent : ViewComponent
+    {
+        public ProdutoCardViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(Produto produto)
+        {
+            return View("Default", produto);
+        }
+    }
+}
