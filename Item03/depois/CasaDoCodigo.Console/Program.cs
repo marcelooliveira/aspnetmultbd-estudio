e6e461505c43d0e75b9708184083717328993b4c
marcelooliveira <mclricardo@gmail.com>
2020-03-08 22:17:25 -0300

    .

--- Item03/depois/CasaDoCodigo.Console/Program.cs
+++ Item03/depois/CasaDoCodigo.Console/Program.cs
@@ -17,31 +17,33 @@ namespace CasaDoCodigo.Console
             IConnectionMultiplexer _redis = ConnectionMultiplexer.Connect("localhost");
             IDatabase _database = _redis.GetDatabase(CARRINHO_DB_INDEX);
 
-            string jsonCarrinho;
-            Carrinho novoCarrinho = null;
+            Carrinho carrinho = null;
             ItemCarrinho novoItemCarrinho = new ItemCarrinho(clienteId, "001", "ASP.NET Core com Múltiplas Bases de Dados", 50.0m, 1);
-            var data = await _database.StringGetAsync("id_do_cliente");
-            if (data.IsNullOrEmpty)
+            var json = await _database.StringGetAsync(clienteId);
+            if (json.IsNullOrEmpty)
             {
-                novoCarrinho = new Carrinho(clienteId);
-                novoCarrinho.Itens.Add(novoItemCarrinho);
-                jsonCarrinho = JsonConvert.SerializeObject(novoCarrinho);
-                await _database.StringSetAsync("id_do_cliente", jsonCarrinho);
+                carrinho = new Carrinho(clienteId);
+                carrinho.Itens.Add(novoItemCarrinho);
+                json = JsonConvert.SerializeObject(carrinho);
+                await _database.StringSetAsync(clienteId, json);
             }
 
-            ImprimirCarrinho(novoCarrinho);
+            json = await _database.StringGetAsync(clienteId);
+            carrinho = JsonConvert.DeserializeObject<Carrinho>(json);
 
-            var json = await _database.StringGetAsync("id_do_cliente");
-            var carrinho = JsonConvert.DeserializeObject<Carrinho>(json);
+            ImprimirCarrinho(carrinho);
 
             var itemCarrinho = 
                 carrinho.Itens.Where(i => i.ProdutoCodigo == "001").Single();
 
             itemCarrinho.Quantidade = 7;
-            jsonCarrinho = JsonConvert.SerializeObject(carrinho);
-            await _database.StringSetAsync("id_do_cliente", jsonCarrinho);
+            json = JsonConvert.SerializeObject(carrinho);
+            await _database.StringSetAsync(clienteId, json);
 
             ImprimirCarrinho(carrinho);
+
+            await _database.KeyDeleteAsync(clienteId);
+
             System.Console.WriteLine("Tecle ENTER para sair...");
             System.Console.ReadLine();
         }
@@ -53,7 +55,7 @@ namespace CasaDoCodigo.Console
             System.Console.WriteLine("=========");
             
             System.Console.WriteLine("Id do cliente: {0}", carrinho.ClienteId);
-            System.Console.WriteLine("Id do cliente: {0:C}", carrinho.Total);
+            System.Console.WriteLine("Total: {0:C}", carrinho.Total);
 
             System.Console.WriteLine("Produtos:");
             System.Console.WriteLine("=========");
@@ -64,86 +66,5 @@ namespace CasaDoCodigo.Console
             }
             System.Console.WriteLine();
         }
-
-
-        //public RedisCarrinhoRepository(ILogger<RedisCarrinhoRepository> logger, IConnectionMultiplexer redis)
-        //{
-        //    _redis = redis;
-        //    _database = redis.GetDatabase(CARRINHO_DB_INDEX);
-        //}
-
-        //public async Task<bool> DeleteCarrinhoAsync(string id)
-        //{
-        //    return await _database.KeyDeleteAsync(id);
-        //}
-
-        //public async Task<Carrinho> GetCarrinhoAsync(string clienteId)
-        //{
-        //    if (string.IsNullOrWhiteSpace(clienteId))
-        //        throw new ArgumentException();
-
-        //    var data = await _database.StringGetAsync(clienteId);
-        //    if (data.IsNullOrEmpty)
-        //    {
-        //        return await UpdateCarrinhoAsync(new Carrinho(clienteId));
-        //    }
-        //    return JsonConvert.DeserializeObject<Carrinho>(data);
-        //}
-
-        //public IEnumerable<string> GetUsuarios()
-        //{
-        //    var server = GetServer();
-        //    return server.Keys()?.Select(k => k.ToString());
-        //}
-
-        //public async Task<Carrinho> AddItemCarrinhoAsync(string clienteId, ItemCarrinho item)
-        //{
-        //    if (item == null)
-        //        throw new ArgumentNullException();
-
-        //    if (string.IsNullOrWhiteSpace(item.ProdutoCodigo))
-        //        throw new ArgumentException();
-
-        //    if (item.Quantidade <= 0)
-        //        throw new ArgumentOutOfRangeException();
-
-        //    var carrinho = await GetCarrinhoAsync(clienteId);
-        //    ItemCarrinho itemDB = carrinho.Itens.Where(i => i.ProdutoCodigo == item.ProdutoCodigo).SingleOrDefault();
-        //    if (itemDB == null)
-        //    {
-        //        itemDB = new ItemCarrinho(item.Id, item.ProdutoCodigo, item.ProdutoNome, item.PrecoUnitario, item.Quantidade);
-        //        carrinho.Itens.Add(item);
-        //    }
-        //    return await UpdateCarrinhoAsync(carrinho);
-        //}
-
-        //public async Task<UpdateQuantidadeOutput> UpdateItemCarrinhoAsync(string customerId, UpdateQuantidadeInput item)
-        //{
-        //    if (item == null)
-        //        throw new ArgumentNullException();
-
-        //    if (string.IsNullOrWhiteSpace(item.Id))
-        //        throw new ArgumentException();
-
-        //    if (item.Quantidade < 0)
-        //        throw new ArgumentOutOfRangeException();
-
-        //    var basket = await GetCarrinhoAsync(customerId);
-        //    ItemCarrinho itemDB = basket.Itens.Where(i => i.ProdutoCodigo == item.Id).SingleOrDefault();
-        //    itemDB.Quantidade = item.Quantidade;
-        //    if (item.Quantidade == 0)
-        //    {
-        //        basket.Itens.Remove(itemDB);
-        //    }
-        //    Carrinho customerBasket = await UpdateCarrinhoAsync(basket);
-        //    return new UpdateQuantidadeOutput(itemDB, customerBasket);
-        //}
-
-        //private IServer GetServer()
-        //{
-        //    var endpoints = _redis.GetEndPoints();
-        //    return _redis.GetServer(endpoints.First());
-        //}
-
     }
 }
